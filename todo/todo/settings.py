981613f587d9d8b9e29b2d471d7b8ad53cb13f16
Uegene Palimov <76953240+EvgenPalimov@users.noreply.github.com>
2022-04-06 23:06:47 +0500

    work to docker v2.17

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = 'todo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ os.path.join(BASE_DIR / '/frontend/build/') ],
+        'DIRS': [ os.path.join(BASE_DIR / '../frontend/build/') ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -140,7 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR / '/frontend/build/static/',)
+STATIC_ROOT = os.path.join(BASE_DIR / '/..frontend/build/static/',)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
