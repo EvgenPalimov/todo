71987312e243606b49a92766821449979af7ef85
Uegene Palimov <76953240+EvgenPalimov@users.noreply.github.com>
2022-04-06 22:57:56 +0500

    work to docker v2.16

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -72,7 +71,7 @@ ROOT_URLCONF = 'todo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(SETTINGS_PATH, 'frontend/build')],
+        'DIRS': [ os.path.join(BASE_DIR / '/frontend/build/') ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -140,8 +139,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR / '../../frontend/build/static/',)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR / '/frontend/build/static/',)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
