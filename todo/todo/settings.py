5f0f85857a4acb5073298c3ad1d23c9bd55e882a
Uegene Palimov <76953240+EvgenPalimov@users.noreply.github.com>
2022-04-06 23:17:29 +0500

    work to docker v2.19

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = 'todo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ BASE_DIR / '../../frontend/build'],
+        'DIRS': [ os.path.join(BASE_DIR / 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -139,8 +139,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = (BASE_DIR / '../../frontend/build/static/',)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR / 'static',)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
