b85a3230f6e865ab2a6611034a57e66f310baa38
Uegene Palimov <76953240+EvgenPalimov@users.noreply.github.com>
2022-04-06 16:25:42 +0500

    work to docker v1.2

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -89,10 +89,21 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db',
+        'USERNAME': 'uegene',
+        'PASSWORD': '1',
+        'HOST': 'db',
+        'PORT': '5432'
     }
 }
 
