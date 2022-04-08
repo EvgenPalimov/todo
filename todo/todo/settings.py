dfee37cab38abebd067db9e8f1df2b026bd98949
Uegene Palimov <76953240+EvgenPalimov@users.noreply.github.com>
2022-04-08 19:09:03 +0500

    work to docker v2.44

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -27,14 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-# CORS_ALLOWED_ORIGINS = [
-#     'http://localhost:8000',
-#     'http://127.0.0.1:8000',
-#     'http://0.0.0.0:8000',
-#     'http://89.108.76.97:8000',
-# ]
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+    'http://0.0.0.0:8000',
+    'http://89.108.76.97:8000',
+]
 
-CORS_ALLOW_ALL_ORIGINS = True
 
 
 
