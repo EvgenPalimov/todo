8376c68f9e88b41602a607a9450d9a459d352dba
Uegene Palimov <76953240+EvgenPalimov@users.noreply.github.com>
2022-04-08 11:51:12 +0500

    work to docker v2.26

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -142,10 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR / '../frontend/build/static/',)
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = (BASE_DIR / '../../frontend/build/static/',)
 
 AUTH_USER_MODEL = 'usersapp.User'
 
