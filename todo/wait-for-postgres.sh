53a85ceaab2e0a451c08890f7061ad1e22bf268d
Uegene Palimov <76953240+EvgenPalimov@users.noreply.github.com>
2022-04-08 18:18:20 +0500

    work to docker v2.37

--- todo/wait-for-postgres.sh
+++ todo/wait-for-postgres.sh
@@ -4,9 +4,10 @@ set -e
 host="$1"
 shift
 cmd="$@"
-until PGPASSWORD="12345qwe" psql -h "db" -d "db" -U "root" -c '\q';>
->&2 echo "Postgres is unavailable - sleeping"
-sleep 1
+
+until PGPASSWORD="12345qwe" psql -h "db" -d "db" -U "root" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
 done
->&2 echo "Postgres is up - executing command"
-exec $cmd
+  >&2 echo "Postgres is up - executing command"
+  exec $cmd
