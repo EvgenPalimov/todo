416a7c538b5514c0fde3df84f921fcf5acfa1972
Uegene Palimov <76953240+EvgenPalimov@users.noreply.github.com>
2022-04-09 11:07:59 +0500

    Work to project to the end v.6

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -44,7 +44,7 @@ class App extends React.Component {
         const data = {name: name, description: description, repository: repository, users: user}
         axios.post(get_url(`projects/`), data, {headers}).then(
             response => {
-                window.location.href = '/projects/';
+                this.loadDataProjects(headers)
             }
         ).catch(error => {
             console.log(error)
@@ -57,7 +57,7 @@ class App extends React.Component {
         const data = {id: id, name: name, description: description, repository: repository, users: user}
         axios.put(get_url(`projects/${id}/`), data, {headers}).then(
             response => {
-                window.location.href = `/projects/`;
+                this.loadDataProjects(headers)
             }
         ).catch(error => {
             console.log(error)
@@ -80,7 +80,7 @@ class App extends React.Component {
         const data = {project: project, text: text, user: user}
         axios.post(get_url(`todo/`), data, {headers}).then(
             response => {
-                window.location.href = '/todo/';
+                this.loadDataToDo(headers)
             }
         ).catch(error => {
             console.log(error)
@@ -93,7 +93,7 @@ class App extends React.Component {
         const data = {project: project, text: text, user: user}
         axios.put(get_url(`todo/${id}/`), data, {headers}).then(
             response => {
-                window.location.href = '/todo/';
+                this.loadDataToDo(headers)
             }
         ).catch(error => {
             console.log(error)
@@ -130,7 +130,6 @@ class App extends React.Component {
                 cookies.set('refresh', response.data.refresh);
                 this.setState({'auth': {username: username, isLogin: true}});
                 this.loadData();
-                window.location.href = '/projects/';
             }).catch(error => {
             if (error.response.status === 401) {
                 alert('Неверный логин или пароль');
@@ -145,7 +144,6 @@ class App extends React.Component {
         cookies.set('access', '')
         cookies.set('refresh', '')
         this.setState({'auth': {username: '', isLogin: false}})
-        window.location.href = '/login'
     }
 
     get_headers() {
--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -143,8 +143,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = (BASE_DIR / 'frontend/build/static/',)
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 AUTH_USER_MODEL = 'usersapp.User'
 
