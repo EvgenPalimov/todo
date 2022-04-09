674ccb654812a5ef85315a34844674b9648efcf3
Uegene Palimov <76953240+EvgenPalimov@users.noreply.github.com>
2022-04-09 11:24:41 +0500

    Work to project to the end v.8

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'frontend/build/static/',)
+STATIC_ROOT = os.path.join(BASE_DIR, '../frontend/build/static/',)
 
 AUTH_USER_MODEL = 'usersapp.User'
 
