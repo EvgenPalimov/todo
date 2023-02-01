6f5374a89067681c15c165504cc8aa114392d1d2
palim <palbimov@mail.ru>
2023-02-01 16:35:46 +0500

    run to server v.1

--- todo/usersapp/management/commands/create_users.py
+++ todo/usersapp/management/commands/create_users.py
@@ -5,6 +5,7 @@ from usersapp.models import User
 
 FAKE = Faker()
 
+
 class Command(BaseCommand):
     help = 'Create Superuser and some test users'
 
@@ -12,14 +13,13 @@ class Command(BaseCommand):
         parser.add_argument('count', type=int)
 
     def handle(self, *args, **options):
-
         # Delete all users
         User.objects.all().delete()
 
         # Create superuser
         User.objects.create_superuser(
-            username='uegene', first_name='Uegene',last_name='Pavlovich',
-            email='uegene@mail.ru', password='1')
+            username='admin', first_name='Uegene', last_name='Pavlovich',
+            email='uegene@mail.ru', password='admin')
         user_count = options['count']
 
         # Create user data
@@ -28,9 +28,10 @@ class Command(BaseCommand):
         emails = [FAKE.unique.last_name() for _ in range(user_count)]
         passwords = [FAKE.unique.first_name() for _ in range(user_count)]
 
-        #Create test users
+        # Create test users
 
-        for f_name, l_name, email, password in zip(first_name, last_name, emails, passwords):
+        for f_name, l_name, email, password in zip(first_name, last_name,
+                                                   emails, passwords):
             username = f'{f_name.lower()}_{l_name.lower()}'
             email = f'{l_name.lower()}@mail.ru'
             User.objects.create_user(
