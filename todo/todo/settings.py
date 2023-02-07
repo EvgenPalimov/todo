5ac6fb49785cdfcbf9d1256cc3d208ac812843cf
palim <palbimov@mail.ru>
2023-02-08 00:13:55 +0500

    frontend build v.7

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -33,7 +33,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:8000',
     'http://127.0.0.1:8000',
     'http://0.0.0.0:8000',
-    'http://46.19.64.201:8000',
+    'http://46.19.64.201',
 ]
 
 # Application definition
